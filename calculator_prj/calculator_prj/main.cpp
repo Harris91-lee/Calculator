11739dca3bb9547a6bbc0ab5daca58ab2817000a
Mingu <mk.kang@haanvision.com>
2023-06-24 18:59:50 +0900

    메인함수 동작 추가 1. 어떤 연산을 실행할지 사용자로 부터 입력받음. 2. 연산할 숫자 입력 받음. 3. 연산 후 결과 출력.

--- calculator_prj/calculator_prj/main.cpp
+++ calculator_prj/calculator_prj/main.cpp
@@ -1,9 +1,43 @@
 #include <iostream>
+#include "operation.h"
+
+enum OPERATOR { PLUS = 1, MINUS, MULTIPLY };
 
 int main(int argc, char* argv[])
 {
-	std::cout << "Calculator" << std::endl;
-	std::cout << "1.plus, 2.minus" << std::endl;
+	int oper,val1,val2;
+	int result;
+
+	do {
+		std::cout << "-----------------------------------" << std::endl;
+		std::cout << "Calculator" << std::endl;
+		std::cout << "  0.exit\n 1.plus\n 2.minus\n 3.multiply\n 4.division\n 5.proportion" << std::endl;
+
+		std::cout << "choose operation(0 ~ 5) : ";
+		std::cin >> oper;
+		if (oper == 0)
+			break;
+
+		std::cout << "value 1 : ";
+		std::cin >> val1;
+		std::cout << "value 2 : ";
+		std::cin >> val2;
+
+		switch (oper) {
+		case PLUS :
+			operation::sum(val1, val2, result);
+			break;
+		case MINUS:
+			operation::sub(val1, val2, result);
+			break;
+		case MULTIPLY:
+			operation::multiply(val1, val2, result);
+			break;
+		}
+
+		std::cout << "result : " << result << std::endl;
+		std::cout << "-----------------------------------" << std::endl;
+	} while (true);
 
 	return 0;
 }
