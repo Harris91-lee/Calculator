3fe1830c44897c5006ba7f76a817a93f1019fa5a
harris_91 <harrislee1020@gmail.com>
2023-07-01 17:23:44 +0900

    static로 바뀜

--- calculator_prj/calculator_prj/operation.cpp
+++ calculator_prj/calculator_prj/operation.cpp
@@ -1,22 +1,22 @@
 #include "operation.h"
 
 
-void operation::sum(int val1, int val2, int result) {
+void operation::sum(int val1, int val2, static int result) {
 	result = val1 + val2;
 }
 
-void operation::sub(int val1, int val2, int result) {
+void operation::sub(int val1, int val2, static int result) {
 	result = val1 - val2;
 }
 
-void operation::div(int val1, int val2, float result) {
+void operation::div(int val1, int val2, static float result) {
 	result = val1 / val2;;
 }
 
-void operation::multiply(int val1, int val2, int result) {
+void operation::multiply(int val1, int val2, static int result) {
 	result = val1 * val2;;
 }
 
-void operation::proportion(int val1, int val2, float result) {
+void operation::proportion(int val1, int val2, static float result) {
 	result = val1 % val2;;
 }
