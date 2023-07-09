65ff4098dc74bf82d1bffbdb2d66a98a32dd6e5a
Mingu <mk.kang@haanvision.com>
2023-07-09 18:36:46 +0900

    숫자 입력받고 디스플레이

--- calculator_ui/calculator_ui/main_window.cpp
+++ calculator_ui/calculator_ui/main_window.cpp
@@ -4,7 +4,70 @@ MainWindow::MainWindow(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
+
+	cur_text.clear();
+	value.clear();
 }
 
 MainWindow::~MainWindow()
 {}
+
+void MainWindow::on_pushButton_one_clicked()
+{
+	cur_text += '1';
+	ui.lineEdit->setText(cur_text);
+}
+
+void MainWindow::on_pushButton_two_clicked()
+{
+	cur_text += '2';
+	ui.lineEdit->setText(cur_text);
+}
+
+void MainWindow::on_pushButton_three_clicked()
+{
+	cur_text += '3';
+	ui.lineEdit->setText(cur_text);
+}
+
+void MainWindow::on_pushButton_four_clicked()
+{
+	cur_text += '4';
+	ui.lineEdit->setText(cur_text);
+}
+
+void MainWindow::on_pushButton_five_clicked() 
+{
+	cur_text += '5';
+	ui.lineEdit->setText(cur_text);
+}
+
+void MainWindow::on_pushButton_six_clicked() 
+{
+	cur_text += '6';
+	ui.lineEdit->setText(cur_text);
+}
+
+void MainWindow::on_pushButton_seven_clicked() 
+{
+	cur_text += '7';
+	ui.lineEdit->setText(cur_text);
+}
+
+void MainWindow::on_pushButton_eight_clicked() 
+{
+	cur_text += '8';
+	ui.lineEdit->setText(cur_text);
+}
+
+void MainWindow::on_pushButton_nine_clicked() 
+{
+	cur_text += '9';
+	ui.lineEdit->setText(cur_text);
+}
+
+void MainWindow::on_pushButton_zero_clicked() 
+{
+	cur_text += '0';
+	ui.lineEdit->setText(cur_text);
+}
--- calculator_ui/calculator_ui/main_window.h
+++ calculator_ui/calculator_ui/main_window.h
@@ -13,4 +13,14 @@ public:
 
 private:
 	Ui::MainWindowClass ui;
+	void on_pushButton_one_clicked();
+	void on_pushButton_two_clicked();
+	void on_pushButton_three_clicked();
+	void on_pushButton_four_clicked();
+	void on_pushButton_five_clicked();
+	void on_pushButton_six_clicked();
+	void on_pushButton_seven_clicked();
+	void on_pushButton_eight_clicked();
+	void on_pushButton_nine_clicked();
+	void on_pushButton_zero_clicked();
 };
